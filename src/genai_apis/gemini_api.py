f205da524882dec0ce34b64cf1a4a9eacbc901f2
Chansung Park <deep.diver.csp@gmail.com>
2024-07-03 00:09:05 +0000

    add safety settings for gemini

--- src/genai_apis/gemini_api.py
+++ src/genai_apis/gemini_api.py
@@ -1,7 +1,6 @@
 import asyncio
 from .abstract import TextGenerationAPI
 
-
 async def _word_generator(sentence):
     for word in sentence.split(" "):
         yield word + " "
@@ -20,14 +19,22 @@ class GeminiAPI(TextGenerationAPI):
         import google.generativeai as genai
 
         stream = False
+        safety_settings = None
         if "stream" in kwargs:
             stream = kwargs.pop("stream")
+
+        if "safety_settings" in kwargs:
+            safety_settings = kwargs.pop("safety_settings")
+
         generation_config = genai.types.GenerationConfig(**kwargs)
         model = genai.GenerativeModel(
             model_name=model, system_instruction=system_instruction
         )
         response = await model.generate_content_async(
-            [prompt], generation_config=generation_config, stream=stream
+            [prompt], 
+            generation_config=generation_config, 
+            safety_settings=safety_settings,
+            stream=stream
         )
 
         if "stream" in kwargs and kwargs["stream"] is True:
@@ -41,13 +48,29 @@ class GeminiVertexAPI(TextGenerationAPI):
         from vertexai.generative_models import GenerativeModel
         from vertexai import generative_models
 
+        DEFAULT_SAFETY_SETTINGS = {
+            generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        }
+
         stream = False
+        safety_settings = None
+
         if "stream" in kwargs:
             stream = kwargs.pop("stream")
+            
+        if "safety_settings" in kwargs:
+            safety_settings = kwargs.pop("safety_settings")
+
         generation_config = generative_models.GenerationConfig(**kwargs)
         model = GenerativeModel(model_name=model, system_instruction=system_instruction)
         response = await model.generate_content_async(
-            [prompt], generation_config=generation_config, stream=stream
+            [prompt], 
+            generation_config=generation_config, 
+            safety_settings=safety_settings,
+            stream=stream
         )
 
         if "stream" in kwargs and kwargs["stream"] is True:
